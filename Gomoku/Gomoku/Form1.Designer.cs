da5485149f50b48df3cf9c26c64b50484f172992
Chanchan <c-yeo@cichdgroup.com>
2024-08-30 15:41:08 +0900

    20240830test

---
+++ Gomoku/Gomoku/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Gomoku
+{
+    partial class Form1
+    {
+        STONE[,] goban = new STONE[19, 19];
+        bool flag = false; // false = black , true = white
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.undoToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newGameToolStripMenuItem.Text = "new game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.viewToolStripMenuItem.Text = "view";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 422);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.undoToolStripMenuItem.Text = "undo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Gomoku";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+    }
+}
+
---
+++ Gomoku/Gomoku/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gomoku
+{
+    enum STONE { none, black, white };
+
+    public partial class Form1 : Form
+    {
+        int margin = 40;　//親メニューとの間隔
+        int distance = 28;　//線と線の間の距離
+        int stoneSize = 27; //碁石の大きさ
+        int stoneCnt = 1;
+
+        List<Save> lstSave = new List<Save> ();
+        bool saveFlag = false;
+
+        Graphics g;
+        Pen pen;
+        Brush wBrush, bBrush;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Gomoku";
+            this.BackColor = Color.BlanchedAlmond;
+
+            pen = new Pen(Color.Black);
+            bBrush = new SolidBrush(Color.Black);
+            wBrush = new SolidBrush(Color.White);
+
+            this.ClientSize = new Size(2 * margin + 18 * distance,
+                2 * margin + 18 * distance + menuStrip1.Height);
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawBoard();
+            DrawStone();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (saveFlag == true)
+            {
+                saveGame();
+                return;
+            }
+
+            //マウスクリックの座標を設定
+            int x = (e.X - margin + distance / 2) / distance;
+            int y = (e.Y - margin + distance / 2) / distance;
+
+
+            //マウスのクリックで石を置く
+            //if (goban[x, y] != STONE.none)
+            //    return;
+
+            //石の重複を防止
+            if (x < 0 || x >= 19 || y < 0 || y >= 19 || goban[x, y] != STONE.none)
+                return;
+
+            Rectangle r = new Rectangle(
+                margin + distance * x - (stoneSize / 2),
+                margin + distance * y - (stoneSize / 2),
+                stoneSize, stoneSize);
+
+            //Rectangle r = new Rectangle(
+            //    centerX - (stoneSize / 2),
+            //    centerY - (stoneSize / 2),
+            //    stoneSize, stoneSize);
+
+            //string baseDirectory = Directory.GetCurrentDirectory();
+
+            string imagePath;
+            if (flag == false)
+            {
+                imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "goishi", "kuro.png");
+                Bitmap bmp = new Bitmap(imagePath);
+                g.DrawImage(bmp, r);
+                lstSave.Add(new Save(x, y, STONE.black, stoneCnt));
+                flag = true;
+                goban[x, y] = STONE.black;
+            }
+            else
+            {
+                imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "goishi", "shiro.png");
+                Bitmap bmp = new Bitmap(imagePath);
+                g.DrawImage(bmp, r);
+                lstSave.Add(new Save(x, y, STONE.white, stoneCnt));
+                flag = false;
+                goban[x, y] = STONE.white;
+            }
+
+            stoneCnt++;
+
+            checkWin(x, y);
+
+
+        }
+
+        private void DrawBoard()
+        {
+            //panel1にGraphicsオブジェクト作成
+            g = panel1.CreateGraphics();
+
+            //縦19
+            for (int i = 0; i < 19; i++)
+            {
+                g.DrawLine(pen, new Point(margin + i * distance, margin),
+                    new Point(margin + i * distance, margin + 18 * distance));
+            }
+
+            //横19
+            for (int i = 0; i < 19; i++)
+            {
+                g.DrawLine(pen, new Point(margin, margin + i * distance),
+                    new Point(margin + 18 * distance, margin + i * distance));
+            }
+        }
+
+        //２次元配列に保存されている碁石を呼び出す
+        private void DrawStone()
+        {
+            string imagePath;
+
+            for (int x = 0; x < 19; x++)
+                for (int y = 0; y < 19; y++)
+                    if (goban[x, y] == STONE.black)
+                    {
+                        imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "goishi", "kuro.png");
+                        Bitmap bmp = new Bitmap(imagePath);
+                        g.DrawImage(bmp, margin + x * distance - stoneSize / 2,
+                            margin + y * distance - stoneSize / 2, stoneSize, stoneSize);
+                    }
+                    else if (goban[x, y] == STONE.white)
+                    {
+                        imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "goishi", "shiro.png");
+                        Bitmap bmp = new Bitmap(imagePath);
+                        g.DrawImage(bmp, margin + x * distance - stoneSize / 2,
+                            margin + y * distance - stoneSize / 2, stoneSize, stoneSize);
+                    }
+        }
+
+        //碁盤をリセット
+        private void setGame()
+        {
+            //碁石の状態を初期化
+            flag = false;
+            goban = new STONE[19, 19];
+            //明日確認
+            stoneCnt = 1;
+
+            //for (int x = 0; x < 19; x++)
+            //    for (int y = 0; y < 19; y++)
+            //goban[x, ] = STONE.none;
+
+            //碁盤を再描画
+            this.Refresh();
+
+            if (goban[0, 1] == STONE.none)
+            {
+                //MessageBox.Show("noneです！");
+            }
+        }
+
+        private void saveGame()
+        {
+
+            string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Post mortem");
+            string fileName = DateTime.Now.ToShortDateString() + "-"
+                + DateTime.Now.Hour+DateTime.Now.Minute+".txt";
+            string fileData = Path.Combine(folderPath, fileName);
+            FileStream fileStream = new FileStream(fileData, FileMode.Create);
+            StreamWriter streamWriter = new StreamWriter(fileStream, Encoding.Default);
+
+            foreach (Save item in lstSave)
+            {
+                streamWriter.WriteLine("{0} {1} {2} {3}", item.X, item.Y, item.stone, item.seq);
+            }
+            streamWriter.Close();
+            fileStream.Close();
+        }
+        //メニューから新しいゲームを始める
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setGame();
+        }
+
+
+        //ゲーム終了処理
+        private void WinGame(int x, int y)
+        {
+            saveGame();
+
+            DialogResult end;
+
+            if (goban[x,y] == STONE.black)
+            {
+                end = MessageBox.Show("黒の勝ちです!\n新しいゲームを始めますか？","ゲーム終了",MessageBoxButtons.YesNo);
+            }
+            else
+            {
+                end = MessageBox.Show("白の勝ちです!\n新しいゲームを始めますか？","ゲーム終了",MessageBoxButtons.YesNo);
+            }
+
+            if (end == DialogResult.Yes)
+                setGame();
+            else if (end == DialogResult.No)
+                this.Close();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void checkWin(int x, int y)
+        {
+
+            int cnt = 1;
+
+            //右方向
+            for (int i = x + 1; i < 19; i++)
+                if (goban[i, y] == goban[x, y])
+                    cnt++;
+                else
+                    break;
+
+            //左方向
+            for (int i = x - 1; i >= 0; i--)
+                if (goban[i, y] == goban[x, y])
+                    cnt++;
+                else
+                    break;
+
+            if (cnt >= 5)
+            {
+                WinGame(x, y);
+                return;
+            }
+
+            cnt = 1;
+
+            //上方向
+            for (int j = y - 1; j >= 0; j--)
+                if (goban[x, j] == goban[x, y])
+                    cnt++;
+                else
+                    break;
+
+            //下方向
+            for (int j = y + 1; j < 19; j++)
+                if (goban[x, j] == goban[x, y])
+                    cnt++;
+                else
+                    break;
+
+            if (cnt >= 5)
+            {
+                WinGame(x, y);
+                return;
+            }
+
+            cnt = 1;
+
+            //左上方向
+            for (int i = x - 1, j = y -1; i >= 0 && j >= 0; i-- , j--)
+                    if (goban[i, j] == goban[x, y])
+                        cnt++;
+                    else
+                        break;
+
+            //右下
+            for (int i = x + 1, j = y + 1; i < 19 && j < 19; i++, j++)
+                if (goban[i, j] == goban[x, y])
+                    cnt++;
+                else
+                    break;
+
+            if (cnt >= 5)
+            {
+                WinGame(x, y);
+                return;
+            }
+
+
+            cnt = 1;
+
+            //右上
+
+            for (int i = x + 1, j = y - 1; i < 19 && j >= 0; i++, j--)
+                if (goban[i, j] == goban[x, y])
+                    cnt++;
+                else
+                    break;
+
+            //左下
+
+            for (int i = x - 1, j = y + 1; i >= 0 && j < 19; i--, j++)
+                if (goban[i, j] == goban[x, y])
+                    cnt++;
+                else
+                    break;
+
+            if (cnt >= 5)
+            {
+                WinGame(x, y);
+                return;
+            }
+
+        }
+    }
+}
